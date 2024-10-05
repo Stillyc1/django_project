90ec502d1f96f70bc5132c62c7c704008e882a5f
Alexander_Baranov <Sti_ll@mail.ru>
2024-10-05 17:56:33 +0300

    add bootstrap, html-list

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def show_home(request: HttpRequest):
+    """функция обрабатывает запрос и возвращает html-страницу"""
+    return render(request, "catalog/home.html")
