54b1639a84bd53160205b710c439948b5ec706ad
Alexander_Baranov <Sti_ll@mail.ru>
2024-10-12 17:03:17 +0300

    create superuser, admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# создали админку python manage.py createsuperuser
+# создали классы для отображения моделей в админке
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Отображает модели(таблицу) Категории в админке"""
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
+    search_fields = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Отображает модели(таблицу) Продуктов в админке"""
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('name', 'price', 'category',)
+    search_fields = ('id', 'name', 'category')
