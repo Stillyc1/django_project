87ccd38d7cf722e59976e0c8c1055e8a18c84269
Alexander_Baranov <Sti_ll@mail.ru>
2024-10-27 12:51:53 +0300

    change FBV controls on CBV controls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
 
+from .views import CatalogListView, CatalogDetailView, CatalogTemplateView
 
 app_name = 'catalog'
 
 
 urlpatterns = [
-    path('', views.show_home, name='show_home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>', views.product_detail, name='product')
+    path('', CatalogListView.as_view(), name='show_home'),
+    path('contacts/', CatalogTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>', CatalogDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,31 +1,48 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView, DetailView, TemplateView
+
 from .models import Product
 
 
 # Create your views here.
-def show_home(request: HttpRequest):
-    """функция обрабатывает запрос и возвращает html-страницу"""
-    if request.method == 'GET':
-        products = Product.objects.all()
-        context = {'products': products}
-
-        return render(request, "catalog/home.html", context=context)
+class CatalogListView(ListView):
+    """Класс представления каталога товаров на главной странице"""
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
 
 
-def contacts(request: HttpRequest):
-    """Обрабатываем форму и возвращаем ответ"""
-    if request.method == 'POST':
-        # Получение данных из формы
-        name = request.POST.get('name')
-        message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        # Здесь мы просто возвращаем простой ответ
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'catalog/contacts.html')
+class CatalogDetailView(DetailView):
+    """Класс представления полной информации о товаре, на отдельной странице"""
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
 
 
-def product_detail(request: HttpRequest, pk: int):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product_detail.html', context=context)
+class CatalogTemplateView(TemplateView):
+    """Класс представления обратной связи с заполнением формы"""
+    template_name = "catalog/contacts.html"
+
+
+# def show_home(request: HttpRequest):
+#     """функция обрабатывает запрос и возвращает html-страницу"""
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         context = {'products': products}
+#
+#         return render(request, "catalog/home.html", context=context)
+
+# def contacts(request: HttpRequest):
+#     """Обрабатываем форму и возвращаем ответ"""
+#     if request.method == 'POST':
+#         # Получение данных из формы
+#         name = request.POST.get('name')
+#         message = request.POST.get('message')
+#         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+#         # Здесь мы просто возвращаем простой ответ
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+#     return render(request, 'catalog/contacts.html')
+
+# def product_detail(request: HttpRequest, pk: int):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {'product': product}
+#     return render(request, 'catalog/product_detail.html', context=context)
